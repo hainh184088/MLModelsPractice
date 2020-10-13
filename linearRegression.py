8b73fd8a7ce79ec5fdc2b4a28b1eda5f0ed21225
sinhvien <thomas.nguyen747@gmail.com>
2020-10-13 14:36:30 +0700

    Fix functions

--- linearRegression.py
+++ linearRegression.py
@@ -9,22 +9,25 @@ def loadData(data_path):
     n = data.shape[0]
     X = data[:, 0].reshape(-1,1)
     Y = data[:, 1].reshape(-1,1)
-    X = np.hstack((np.ones((n, 1)), X))
-    w = np.array([0.,1.]).reshape(-1,1)
-    
+
+    # plt.scatter(x,y)
     plt.plot(X, Y, 'ro')
     plt.axis([0, 100, 400, 2000])
     plt.xlabel('square meters')
     plt.ylabel('price')
-    plt.show()
+
+    X = np.hstack((np.ones((n, 1)), X))
+    w = np.array([0.,1.]).reshape(-1,1)
+
+    return X,Y,w,n
 
     
 class linearRegression:
     def __init__(self):
         return
-    def lR(self,X_train,Y_traing,alpha):
+    def lR(self, X_train,Y_train,alpha):
         numOfIteration = 100
-        self.alpha = alpha
+        cost = np.zeros((numOfIteration,1))
         for i in range(1, numOfIteration):
             r = np.dot(X_train, w) - Y_train
             cost[i] = 0.5*np.sum(r*r)
@@ -32,8 +35,13 @@ class linearRegression:
             # correct the shape dimension
             w[1] -= alpha*np.sum(np.multiply(r, X_train[:,1].reshape(-1,1)))
             print(cost[i])
+    def predict(self,X_train,Y_train,w,n):
+        predict = np.dot(X_train,w)
+        plt.plot((X_train[0][1],X_train[n-1][1]),(predict[0],predict[n-1]),'r')
+        plt.show()
 
 if __name__ == '__main__':
-    loadData(data_path='./data_linear.csv')
+    X,Y,w,n = loadData(data_path='./data_linear.csv')
     newLinear = linearRegression()
-    newLinear.lR(X,Y,0.1)
+    newLinear.lR(X,Y,0.0000000000001)
+    newLinear.predict(X,Y,w,n)
