a9492289bec910e584cd91c3b652d410c1a4d55e
Hai Nguyen <thomas.nguyen747@gmail.com>
2020-10-13 10:45:33 +0700

    Change the data file and load data function

--- linearRegression.py
+++ linearRegression.py
@@ -8,7 +8,20 @@ def loadData(data_path):
     data.dropna(subset=['Births by caesarean section (%)','Births attended by skilled health personnel (%)'], inplace=True)
     X = data['Births by caesarean section (%)'].to_numpy()
     Y = data['Births attended by skilled health personnel (%)'].to_numpy()
-    print(Y,X)
+    X = X.reshape((X.shape[0], 1))
+    Y = Y.reshape((Y.shape[0], 1))
+    X = X.T
+    Y = Y.T
+    # print(X.shape)
+    plt.plot(X, Y, 'ro')
+    plt.axis([5, 50, 80, 100])
+    plt.xlabel('Births by caesarean section (%)')
+    plt.ylabel('Births attended by skilled health personnel (%)')
+    plt.show()
+    one = np.ones((X.shape[0], 1))
+    Xbar = np.concatenate((one, X), axis = 0)
+    print(Xbar)
+
     
 class linearRegression:
     def _init_(self):
