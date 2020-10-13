f6ae9b67c234a2025bb85c919da102da52666282
Hai Nguyen <thomas.nguyen747@gmail.com>
2020-10-13 11:04:28 +0700

    Change the data file and load data function

--- linearRegression.py
+++ linearRegression.py
@@ -4,29 +4,36 @@ import matplotlib.pyplot as plt
 
 def loadData(data_path):
     with open(data_path) as file:
-        data = pd.read_csv(file)
-    data.dropna(subset=['Births by caesarean section (%)','Births attended by skilled health personnel (%)'], inplace=True)
-    X = data['Births by caesarean section (%)'].to_numpy()
-    Y = data['Births attended by skilled health personnel (%)'].to_numpy()
-    X = X.reshape((X.shape[0], 1))
-    Y = Y.reshape((Y.shape[0], 1))
-    X = X.T
-    Y = Y.T
-    # print(X.shape)
+        data = pd.read_csv(file).values
+    print(data.dtype)
+    n = data.shape[0]
+    X = data[:, 0].reshape(-1,1)
+    Y = data[:, 1].reshape(-1,1)
+    X = np.hstack((np.ones((n, 1)), X))
+    w = np.array([0.,1.]).reshape(-1,1)
+    
     plt.plot(X, Y, 'ro')
-    plt.axis([5, 50, 80, 100])
-    plt.xlabel('Births by caesarean section (%)')
-    plt.ylabel('Births attended by skilled health personnel (%)')
+    plt.axis([0, 100, 400, 2000])
+    plt.xlabel('square meters')
+    plt.ylabel('price')
     plt.show()
-    one = np.ones((X.shape[0], 1))
-    Xbar = np.concatenate((one, X), axis = 0)
-    print(Xbar)
 
     
 class linearRegression:
-    def _init_(self):
+    def __init__(self):
         return
-    # def
+    def lR(self,X_train,Y_traing,alpha):
+        numOfIteration = 100
+        self.alpha = alpha
+        for i in range(1, numOfIteration):
+            r = np.dot(X_train, w) - Y_train
+            cost[i] = 0.5*np.sum(r*r)
+            w[0] -= alpha*np.sum(r)
+            # correct the shape dimension
+            w[1] -= alpha*np.sum(np.multiply(r, X_train[:,1].reshape(-1,1)))
+            print(cost[i])
 
 if __name__ == '__main__':
-    loadData(data_path='./data.csv')
+    loadData(data_path='./data_linear.csv')
+    newLinear = linearRegression()
+    newLinear.lR(X,Y,0.1)
